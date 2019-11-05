43972b8363aac9403132e675d476c14838fb5e39
V. Diviem <vd@enterego.ru>
2019-11-05 21:43:08 +0300

    A comparison of source and already generated files by checksums is added, regeneration is started only if the source file was really changed. Now all generated images are stored in a separate folder. Fixed a bug with the Cyrillic names of the source files and names with spaces. Added the ability to specify the compression level when invoking the script.

--- README.md
+++ README.md
@@ -1,32 +1,43 @@
 ## How it works?
-For example we have an original image, located there https://test.com/images/horse.jpg
-When user sending request for this file, Nginx Web Server will be looking for exists alternative file with _.webp_ extension, if success, Nginx will response with content and header of alternative _.webp_ file instead original.
+For example we have an original image, located there _test.com/images/horse.jpg_
+When user sending request for this file, Nginx Web Server will be looking for exists alternative file with _.webp_ extension, if success, Nginx will send response with content and header of alternative _.webp_ file instead original.
 This's will be work for images with following extension: _.jpg, .jpeg, .png_ 
 
-#####1. Install webp library on your Linux
+#####1. Install _WebP_ and _ExifTool_ libraries on your Linux
 CentOS: 
-    `yum install libwebp-tools`
+    `yum install libwebp-tools perl-Image-ExifTool`
     
-Ubuntu:
-    `sudo apt-get install webp`
+Debian/Ubuntu:
+    `sudo apt-get install webp exiftool`
     
-#####2. Copy _nginxwebp_ script of this package in to your server
-`cp /bin/nginxwebp.sh /bin/nginxwebp.sh`
+#####2. Copy _nginxwebp.sh_ script of this package in to your server
+`cp ./bin/nginxwebp.sh /bin/nginxwebp.sh`
 
 `chmod +x /bin/nginxwebp.sh`
 
-#####3. Edit your Nginx configuration file 
+#####3. Edit your _Nginx_ configuration file 
 You should to find your active _.conf_ file and edit the _server_ section.
 Usually _.conf_ file is located  in _/etc/nginx/conf.d/default.conf_ but your path may be another.
 Copy the contents (marked by comments) from _/etc/nginx/conf.d/default.conf_ (this packages file) in to your Nginx _.conf_ _server_ section.
 
+Dont forget to change value for _$webpdir_ variable! 
+
 #####4. Start the image conversion processing for the desired directory
-`nginxwebp /your/desired/directory/`
+Where first param _/var/www/html_ is path of your web sites root directory, second param _wp-content/upload_ is relative path of your sites directory with images, third param _webp_ is relative path for compiled _.webp_ files and fourth param _80_ is a compression ratio (in the range of 1 to 100). 
+Dont forget to insert your real paths in this command!
+
+`nginxwebp.sh /var/www/html/ wp-content/upload webp 80`
 
 #####5. Add the processing script in your task scheduler
 This is example of task, starting every 12 hours.
 
 Cron:  
-`* */12 * * * nginxwebp /your/desired/directory/ >> /var/log/cron/cron_nginxwebp.log 2>&1`
+`* */12 * * * nginxwebp.sh /var/www/html/ wp-content/upload webp 80 >> /var/log/cron/cron_nginxwebp.log 2>&1`
 
 #####6. Enjoy! :)
+
+## Whats doing _nginxwebp.sh_ script?
+This script find all images in the given directory (second param) and via _WebP_ library make compressed (compress ratio - fourth param) _.webp_ files in the given directory for output (second param).
+The search is performed recursively, repeating the structure of the source directory in the output directory. 
+
+At the time of compression, the script receives the checksum of the source file and stores the converted _.webp_ in the meta-data of the converted _.webp_, the next time these amounts are checked, and the _.webp_ file is recreated only if they diverge.
--- bin/nginxwebp.sh
+++ bin/nginxwebp.sh
@@ -1,7 +1,106 @@
 #!/bin/bash
-webpbuilder ()
+progress ()
 {
-if [ ! -f "$1.webp" ]; then cwebp -q 80 $1 -o "$1.webp"; fi
+        HASH=($(md5sum "$I_FILE_PATH"))
+        exiftool -rights="$HASH" -overwrite_original_in_place "$1"
+        cwebp -q "$3" -metadata all "$1" -o "$2"
 }
-export -f myfunc
-find $1 -type f \( -iname \*.jpg -o -iname \*.jpeg -o -iname \*.png \) -exec bash -c 'webpbuilder "$@"' bash {} \;
+
+showhelper ()
+{
+        echo -e "\n  ----------------------------------\n  Error: $1 \n  ---------------------------------- \n  How to use this?'\n  nginxwebp.sh [absolutely path of root directory] [relative path of directory for input] [relative path of directory for output] [compression ratio]\n  Example: nginxwebp.sh /var/www/html uploads webp 70"
+}
+
+webpbuilder  ()
+{
+        I_FILE_PATH=$1
+	echo "File: $I_FILE_PATH"
+        O_DIR_PATH=$2
+	RATIO=$3
+	ROOT_DIR=$4
+	ROOT_DIR_LENGTH=${#ROOT_DIR}
+        I_FILE_NAME=$(basename -- "$I_FILE_PATH")
+        RELATIVE_FILE_DIR=($(dirname "$I_FILE_PATH" | cut -c $ROOT_DIR_LENGTH-))
+	RELATIVE_FILE_DIR="${O_DIR_PATH}${RELATIVE_FILE_DIR}/"
+	I_FILE_NAME=$(basename -- "$I_FILE_PATH")
+	O_FILE_PATH="${RELATIVE_FILE_DIR}${I_FILE_NAME}.webp"
+	if [ ! -d $RELATIVE_FILE_DIR ]; then
+                mkdir -p $RELATIVE_FILE_DIR
+        fi
+        if [ -f "$O_FILE_PATH" ]; then
+                I_FILE_HASH=($(exiftool -rights "$I_FILE_PATH" | cut -d: -f2))
+                O_FILE_HASH=($(exiftool -rights "$O_FILE_PATH" | cut -d: -f2))
+                if [ -n $O_FILE_HASH ]; then
+                        if [ $I_FILE_HASH == $O_FILE_HASH ]; then
+                                echo "  Action: SKIP"
+                        else
+                                echo "  Action: REBUILD"
+                                progress "$I_FILE_PATH" "$O_FILE_PATH" "$RATIO"
+                        fi
+                else
+                        progress "$I_FILE_PATH" "$O_FILE_PATH" "$RATIO"
+                fi
+       else
+                echo "  Action: BUILD"
+                progress "$I_FILE_PATH" "$O_FILE_PATH" "$RATIO"
+       fi
+}
+
+export -f progress
+export -f webpbuilder
+
+ROOT_DIR=$1
+I_DIR=$2
+O_DIR=$3
+RATIO=$4
+
+if [ -n $ROOT_DIR ]; then
+	re='\/$'
+        if ! [[ "$ROOT_DIR" =~ $re ]]; then
+        	ROOT_DIR="${ROOT_DIR}/"
+        fi
+	if [ -d $ROOT_DIR ]; then
+		if [ -n $I_DIR ]; then
+			I_DIR="${ROOT_DIR}${I_DIR}"
+        		if [ -d $I_DIR ]; then
+                		if [ -n $O_DIR ]; then
+					O_DIR="${ROOT_DIR}${O_DIR}"
+                        		if [ ! -d $O_DIR ]; then
+                                		mkdir $O_DIR
+                                		if [ $? -ne 0 ]; then
+                                        		showhelper "Output directory can not be created! Please, check permissions!"
+                                		fi
+                        		fi
+                        		if [ -d $O_DIR ]; then
+                                		re='\/$'
+                                		if [[ "$I_DIR" =~ $re ]]; then
+                                        		I_DIR="${I_DIR::-1}"
+                                		fi
+                                		if [[ "$O_DIR" =~ $re ]]; then
+                                        		O_DIR=${O_DIR::-1}
+                                		fi
+                                		re='^[0-9]+$'
+                                		if [[ $RATIO =~ $re ]] && [ $RATIO -ge 1 ] && [ $RATIO -le 100 ]; then
+                                        		echo -e "\n  Configuration is ready!\n  ----------------------------------\n  Directory for processing: $I_DIR \n  Directory for output files: $O_DIR \n  Compression ratio: $RATIO%\n  ----------------------------------\n  Starting compression... \n";
+                                        		find $I_DIR -type f \( -iname \*.jpg -o -iname \*.jpeg -o -iname \*.png \) -exec bash -c 'webpbuilder "$@"' bash {} $O_DIR $RATIO $ROOT_DIR \;
+
+                                        		echo -e "\n  ----------------------------------\n  Compression process is finished! \n  ----------------------------------"
+                                		else
+                                        		showhelper "Ratio must be a number from 1 to 100!"
+                                		fi
+                        		fi
+                		else
+                       			showhelper "Output Directory Path parameter not supplied!"
+                		fi
+        		else
+                		showhelper "Directory path '$i_DIR' is not correct!"
+        		fi
+		else
+        		showhelper "Input Diretory Path parameter not supplied!"
+		fi
+	else
+		showhelper "Root Directory is not isset!"
+	fi
+else
+	showhelper "Root Diretory Path parameter not supplied!"
+fi
